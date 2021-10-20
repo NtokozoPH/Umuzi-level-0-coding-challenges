295d8feb3bfae33bd64631f97cb3b47c4bcf6f36
NtokozoPH <petros.ntokozoh@gmail.com>
2021-10-20 10:22:42 +0200

    completed task0.2 coding challenge

---
+++ task2.py
@@ -0,0 +1,15 @@
+x = 1 + 1 * 2           # multiplication first, BODMAS
+
+y = (1 + 1) * 2         # Brackets Off first, BODMAS
+
+z = 1 + ( 1 * 2)        # Brackets Off first, BODMAS
+
+a = 1 + 1 * 2 / 2       # Division first, then Multiplication lastly Addition
+
+b = (1 + 1 * 2 ) /  2   # Brackets off first, multplication taking first priority within brackets
+
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
