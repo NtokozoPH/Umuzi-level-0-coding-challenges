e057374c46bad83f67c92cf25c79c99ffe15ba48
NtokozoPH <petros.ntokozoh@gmail.com>
2021-10-20 10:39:48 +0200

    completed task0.7 coding challenge

---
+++ task7.py
@@ -0,0 +1,7 @@
+# a function to convert  Celsius to Fahrenheit
+def cel_to_fah(c):
+    return (c * 9/5) + 32 # Celsius to Fahrenheit
+
+# a function to convert Fahrenheit to Celsius
+def fah_to_cel(f):
+    return ((f - 32) * 5/9) # Fahrenheit to Celsius
