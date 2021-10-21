5b9b48e46c2517690de92d03f0bf2ec847d40007
NtokozoPH <petros.ntokozoh@gmail.com>
2021-10-21 14:21:27 +0200

    fixed function to work o negative integers

--- task6.py
+++ task6.py
@@ -1,5 +1,5 @@
 def maximum(*numbers):
-    max_num = 0            # initialising a maximum number
+    max_num = numbers[0]   # initialising a maximum number as the first number
     for num in numbers:
         if num > max_num:  # checking if current number is greater than current maximum number
             max_num = num  # updating maximum number if less the current number
