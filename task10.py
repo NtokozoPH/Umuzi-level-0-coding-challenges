e8e1f324b85446933abedb92d77123843b14207b
NtokozoPH <petros.ntokozoh@gmail.com>
2021-10-21 14:27:54 +0200

    fixed output display

--- task10.py
+++ task10.py
@@ -1,5 +1,6 @@
 def common_char(string1,string2):
-    set1 = set(string1)                 # converting first string to set
-    set2 = set(string2)                 # converting a second string to a set
-    common_char = tuple(set1 & set2)    # checking for characters in both sets
-    return (common_char)
+    set1 = set(string1)
+    set2 = set(string2)
+    common_char = list(set1 & set2)
+    common_char = ', '.join(common_char)
+    print('Common letters:', common_char)
