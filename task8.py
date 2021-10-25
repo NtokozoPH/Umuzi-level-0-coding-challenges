47365ed1f9937b8b87ae125b081fc44dd84a19b2
NtokozoPH <petros.ntokozoh@gmail.com>
2021-10-25 12:38:49 +0200

    corrected for 0 hours and minutes.

--- task8.py
+++ task8.py
@@ -1,7 +1,7 @@
 def num_to_time(num):
-    hours = num // 60                               # converting a number into hours
-    minutes = num % 60                              # converting a number into minutes
-    if (hours > 1) and (minutes > 1):
+    hours = num // 60
+    minutes = num % 60
+    if (hours > 1 or hours == 0) and (minutes > 1 or hours == 0):
         print(hours,'hours',',',minutes,'minutes')
     elif (hours <= 1) and (minutes <= 1):
         print(hours,'hour',',',minutes,'minute')
