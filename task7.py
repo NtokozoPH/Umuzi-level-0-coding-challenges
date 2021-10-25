f7b85f487b96d09504c27c8265e2ba9e5753f8b2
NtokozoPH <petros.ntokozoh@gmail.com>
2021-10-25 12:54:47 +0200

    removed comments not adding value to code

--- task7.py
+++ task7.py
@@ -1,7 +1,7 @@
 # a function to convert  Celsius to Fahrenheit
 def cel_to_fah(c):
-    return (c * 9/5) + 32 # Celsius to Fahrenheit
+    return (c * 9/5) + 32
 
 # a function to convert Fahrenheit to Celsius
 def fah_to_cel(f):
-    return ((f - 32) * 5/9) # Fahrenheit to Celsius
+    return ((f - 32) * 5/9)
